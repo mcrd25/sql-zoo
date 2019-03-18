251b87a2c397fc98e6601af3b3a640064e07d979
mcrd25 <mayacrd25@gmail.com>
2019-03-18 15:52:36 -0400

    Added questions to 0_select_basics

--- 0_select_basics.sql
+++ 0_select_basics.sql
@@ -1,21 +1,24 @@
 /* Based on https://sqlzoo.net/wiki/SELECT_basics */
 
--- 1
+-- 1. The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
 SELECT population FROM world
 	WHERE name = 'Germany';
 
--- 2
+-- 2. Checking a list The word IN allows us to check if an item is in a list. The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
+			-- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 SELECT name, population FROM world
 	WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
--- 3
+-- 3. Which countries are not too small and not too big? BETWEEN allows range checking (range specified is inclusive of boundary values). The example below shows countries with an area of 250,000-300,000 sq. km. Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
 SELECT name, area FROM world
 	WHERE area BETWEEN 200000 AND 250000
 
 
+
+
 /*Answers to section's quiz called SELECT QUIZ found at  https://sqlzoo.net/wiki/SELECT_Quiz*/
 
-# 1
+# 1. 
 SELECT name, population
 FROM world
 WHERE population BETWEEN 1000000 AND 1250000
